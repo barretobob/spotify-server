2d421fa17694999242c162aadac2e66bf7bdcfa9
barretobob <147617232+barretobob@users.noreply.github.com>
2025-11-24 14:55:18 -0300

    login.js

--- api/login.js
+++ api/login.js
@@ -1,6 +1,9 @@
 export default function handler(req, res) {
   const clientId = process.env.SPOTIFY_CLIENT_ID;
-  const redirectUri = "https://spotify-server-ebon.vercel.app/api/callback";
+
+  // Monta automaticamente o URL de callback baseado no domínio atual no Vercel
+  const redirectUri = `${req.headers['x-forwarded-proto']}://${req.headers['x-forwarded-host']}/api/callback`;
+
   const scope = [
     "user-read-playback-state",
     "user-modify-playback-state",
@@ -14,28 +17,24 @@ export default function handler(req, res) {
     return res.status(400).json({ error: "Missing Spotify Client ID!" });
   }
 
-  // Geração de uma string aleatória para proteção contra CSRF
+  // Gerar state (CSRF)
   const state = generateRandomString(16);
-  res.setHeader('Set-Cookie', `spotify_auth_state=${state}; HttpOnly; Path=/; Max-Age=600`);
+  res.setHeader("Set-Cookie", `spotify_auth_state=${state}; HttpOnly; Path=/; Max-Age=600`);
 
   const params = new URLSearchParams({
-    response_type: 'code',
+    response_type: "code",
     client_id: clientId,
     scope: scope,
     redirect_uri: redirectUri,
     state: state
   });
 
-  const authorizeUrl = `https://accounts.spotify.com/authorize?${params.toString()}`;
-  
-  // Redireciona para a página de login do Spotify
-  res.redirect(authorizeUrl);
+  return res.redirect(`https://accounts.spotify.com/authorize?${params}`);
 }
 
-// Função auxiliar para gerar uma string aleatória
 function generateRandomString(length) {
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
   for (let i = length; i > 0; --i) {
     result += chars[Math.floor(Math.random() * chars.length)];
   }
