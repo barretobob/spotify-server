4c4765322fa25bb7cf36c333d2546ead61d5b6e4
barretobob <147617232+barretobob@users.noreply.github.com>
2025-11-23 23:53:13 -0300

    login.js

--- api/login.js
+++ api/login.js
@@ -1,6 +1,10 @@
 export default function handler(req, res) {
   const clientId = process.env.SPOTIFY_CLIENT_ID;
   const redirectUri = "https://spotify-server-ebon.vercel.app/api/callback";
+
+  // 🔥 state único para cada login
+  const state = Math.random().toString(36).substring(2);
+
   const scope = [
     "user-read-playback-state",
     "user-modify-playback-state",
@@ -13,8 +17,9 @@ export default function handler(req, res) {
   const params = new URLSearchParams({
     response_type: "code",
     client_id: clientId,
-    scope: scope,
-    redirect_uri: redirectUri
+    scope,
+    redirect_uri: redirectUri,
+    state
   });
 
   res.redirect("https://accounts.spotify.com/authorize?" + params.toString());
